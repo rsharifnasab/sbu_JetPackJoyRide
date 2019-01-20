8cdb659425b7a126a16bb39619d9e029e61da80c
Roozbeh Sharifnasab <rsharifnasab@gmail.com>
2019-01-20 14:21:54 +0330

    load paralel background

--- background.h
+++ background.h
@@ -4,7 +4,21 @@ enum bg_steps {start,one,two};
 enum bs_types {lab,volca,paral};
 Texture background[3][3];
 
-bool load_background_texture() // TODO
+enum paral_type {back,front};
+Texture parals[2][2];
+
+bool load_paral()
+{
+  parals[front][0]= SBDL::loadTexture( "./assets/pic/parallex/First P.png" );
+  parals[front][1]= SBDL::loadTexture( "./assets/pic/parallex/First P2.png" );
+
+  parals[back][1]= SBDL::loadTexture( "./assets/pic/parallex/Second P2.png" );
+  parals[back][1]= SBDL::loadTexture( "./assets/pic/parallex/Second P2.png" );
+
+  return true;
+}
+
+bool load_background_texture()
 {
   background[lab][start] = SBDL::loadTexture( "./assets/pic/back/Lab Start.png" );
   background[lab][one] = SBDL::loadTexture( "./assets/pic/back/Lab1.png" );
@@ -18,6 +32,7 @@ bool load_background_texture() // TODO
   background[paral][one] = SBDL::loadTexture( "./assets/pic/back/warehouse.png" );
   background[paral][two] = SBDL::loadTexture( "./assets/pic/back/warehouse.png" );
 
+  load_paral();
   return true;
 }
 
